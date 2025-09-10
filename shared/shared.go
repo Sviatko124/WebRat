ecbf44d75ad2a0ea07ed5cdcd78db30baffa156d
Sviatko124 <55671595+Sviatko124@users.noreply.github.com>
2025-09-10 17:07:21 -0400

    Add files via upload

--- shared/shared.go
+++ shared/shared.go
@@ -1,1 +1,27 @@
+package shared
 
+import (
+    "sync"
+    "net/http"
+)
+
+const (
+    RequestBufferSize = 100
+)
+
+var (
+    OutputHeader  http.Header
+    OutputURL     string
+    Mu           sync.RWMutex
+    RequestChan  = make(chan *http.Request, RequestBufferSize)
+    ResponseChan = make(chan *http.Request, RequestBufferSize)
+)
+
+func ClearBuffers() {
+    for len(RequestChan) > 0 {
+        <-RequestChan
+    }
+    for len(ResponseChan) > 0 {
+        <-ResponseChan
+    }
+}
